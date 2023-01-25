345d310502ad248a8bf2ad34be91f177bf1451a7
armzig <bellotaophyc@outlook.com>
2023-01-25 21:55:48 +0100

    25 01 2023 Implementing the stack as a list

---
+++ stackModule.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# stack Module
+def getStack():
+
+    """Creates and returns an empty stack."""
+    
+    return []
+
+def isEmpty(s):
+
+    """Returns True if stack empty, otherwise returns False."""
+
+    if s == []:
+        return True
+    else:
+        return False
+
+def top(s):
+
+    """Returns value of the top item of stack, if stack not empty.
+       Otherwise, returns None.
+    """
+
+    if isEmpty(s):
+        return None
+    else:
+        return s[len(s) - 1]
+
+def push(s, item):
+
+    """Pushes item on the top of stack. """
+
+    s.append(item)
+
+def pop(s):
+
+    """Returns top os stack if stack not empty. Otherwise, returns None. """
+
+    if isEmpty(s):
+        return None
+    else:
+        item = s[len(s) - 1]
+        del s[len(s) - 1]
+        return item
